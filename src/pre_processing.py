d1f3269dd183e6d42dfa0dab2db5c80ddf5f1894
Andy Lambert <andyalambert@gmail.com>
2020-03-20 18:28:41 -0400

    extreme smoothing and desaturation

---
+++ src/pre_processing.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+
+import cv2
+import numpy as np
+import sys
+
+def remove_noise(img):
+    denoised = cv2.fastNlMeansDenoisingColored(img, None, 21, 21, 7, 21)
+    return denoised
+
+def desaturate(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    desaturated = np.zeros(hsv.shape, hsv.dtype)
+    for j in range(hsv.shape[0]):
+        for i in range(hsv.shape[1]):
+            new_s = hsv[j, i, 1] - 50
+            if new_s < 0:
+                new_s = 0
+            new_hsv = [hsv[j, i, 0],
+                       new_s,
+                       hsv[j, i, 2]]
+            desaturated[j, i] = new_hsv
+    bgr = cv2.cvtColor(desaturated, cv2.COLOR_HSV2BGR)
+    return bgr
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    img = cv2.imread(filename)
+    after_noise = remove_noise(img)
+    after_desaturation = desaturate(after_noise)
+    cv2.imshow("Processed image", after_desaturation)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
