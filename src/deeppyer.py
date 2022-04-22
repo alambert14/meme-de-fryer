dd54265d65871527423a6d149e1d7153e55802d6
Amanda Liu <lamanda@mit.edu>
2022-04-22 18:08:47 -0400

    Add reasonable frying strategy

--- src/deeppyer.py
+++ src/deeppyer.py
@@ -10,6 +10,7 @@ from typing import Tuple
 from PIL import Image, ImageOps, ImageEnhance, ImageFilter
 import cv2
 import numpy
+import skimage
 
 __all__ = ('Colour', 'ColourTuple', 'DefaultColours', 'deepfry')
 
@@ -25,7 +26,6 @@ class DefaultColours:
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-#flare_img = Image.open(BytesIO(pkgutil.get_data(__package__, 'flare.png')))
 
 FlarePosition = namedtuple('FlarePosition', ['x', 'y', 'size'])
 
@@ -35,12 +35,12 @@ def add_salt_and_pepper(image, amount):
 
     # add salt
     nb_salt = numpy.ceil(amount * output.size * 0.5)
-    coords = [numpy.random.randint(0, i - 1, int(nb_salt)) for i in output.shape]
+    coords = [numpy.random.randint(0, i - 1, int(nb_salt)) for i in output.shape[:2]]
     output[coords] = 1
 
     # add pepper
     nb_pepper = numpy.ceil(amount* output.size * 0.5)
-    coords = [numpy.random.randint(0, i - 1, int(nb_pepper)) for i in output.shape]
+    coords = [numpy.random.randint(0, i - 1, int(nb_pepper)) for i in output.shape[:2]]
     output[coords] = 0
 
     return Image.fromarray(output)
@@ -63,11 +63,11 @@ async def deepfry(img: Image, *, colours: ColourTuple = DefaultColours.red, flar
     `Image`
         Deepfried image.
     """
-    SHARPNESS = 4
-    CONTRAST = 2
-    BRIGHTNESS = 2
+    SHARPNESS = 3
+    CONTRAST = 5
+    BRIGHTNESS = 4
     SATURATION = 1.5
-    NOISE = 0.05
+    NOISE = 2
  
     img = img.copy().convert('RGB')
     # flare_positions = []
@@ -89,45 +89,23 @@ async def deepfry(img: Image, *, colours: ColourTuple = DefaultColours.red, flar
     img = saturation.enhance(SATURATION)
 
     # NOISE
-    img = add_salt_and_pepper(img,NOISE)
+    skimg = numpy.asarray(img)
+    for i in range(NOISE):
+        skimg = skimage.util.random_noise(skimg, mode="gaussian")
+    skimg = (255*skimg).astype(numpy.uint8)
+    img = Image.fromarray(skimg)
 
-    """
-    if flares:
-        opencv_img = cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2GRAY)
-
-        faces = face_cascade.detectMultiScale(
-            opencv_img,
-            scaleFactor=1.3,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-        for (x, y, w, h) in faces:
-            face_roi = opencv_img[y:y+h, x:x+w]  # Get region of interest (detected face)
-
-            eyes = eye_cascade.detectMultiScale(face_roi)
-
-            for (ex, ey, ew, eh) in eyes:
-                eye_corner = (ex + ew / 2, ey + eh / 2)
-                flare_size = eh if eh > ew else ew
-                flare_size *= 4
-                corners = [math.floor(x) for x in eye_corner]
-                eye_corner = FlarePosition(*corners, flare_size)
-
-                flare_positions.append(eye_corner)
-    """
-
+    # img = img.filter(ImageFilter.UnsharpMask(radius = 3, percent = 200, threshold = 5))
     # Crush image to hell and back
-    """
     img = img.convert('RGB')
     width, height = img.width, img.height
-    img = img.resize((int(width ** .75), int(height ** .75)), resample=Image.LANCZOS)
-    img = img.resize((int(width ** .88), int(height ** .88)), resample=Image.BILINEAR)
-    img = img.resize((int(width ** .9), int(height ** .9)), resample=Image.BICUBIC)
+    img = img.resize((int(width ** .95), int(height ** .95)), resample=Image.LANCZOS)
+    img = img.resize((int(width ** .95), int(height ** .95)), resample=Image.BICUBIC)
     img = img.resize((width, height), resample=Image.BICUBIC)
     img = ImageOps.posterize(img, 4)
-    """
+    
+    # BLUR
+    # img = img.filter(ImageFilter.GaussianBlur(radius = 1))
 
     # Generate colour overlay
     """
@@ -140,13 +118,6 @@ async def deepfry(img: Image, *, colours: ColourTuple = DefaultColours.red, flar
     # Overlay red and yellow onto main image and sharpen the hell out of it
     img = Image.blend(img, r, 0.75)
     img = ImageEnhance.Sharpness(img).enhance(100.0)
-    """
-
-    # Apply flares on any detected eyes
-    """
-    for flare in flare_positions:
-        flare_transformed = flare_img.copy().resize((flare.size,) * 2, resample=Image.BILINEAR)
-        img.paste(flare_transformed, (flare.x, flare.y), flare_transformed)
     """
 
     return img
